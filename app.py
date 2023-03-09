72fdc28c767e0b8cec2de0228bacc30336d09124
Jens <jens.papenhagen@web.de>
2023-03-09 17:35:05 +0100

    enhance requirements.txt file and cleanup app.py

--- app.py
+++ app.py
@@ -1,108 +1,89 @@
 from flask import Flask, request, jsonify, Response
 from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
 import torch
+from urllib.request import urlopen
 from PIL import Image
 import os
-import requests
-import shutil
-import re
-
 
 app = Flask(__name__)
 
+
 # Example:
 # http://127.0.0.1:5000/local?data=test.png
-@app.route('/local', methods=['GET', 'POST'])
-def local() -> Response:
-    if request.method == "GET":
-        data: str = request.args.get('data')
-        if data is None:
-            return jsonify("Please input a valid string: /local?data=test.png")
-        print("data ---- > ", data)
-        realtiv_path = "images/" + data
-        imagePath = os.path.join(os.getcwd(), realtiv_path)
+@app.route('/local', methods=['GET'])
+def local() -> Response:    
+    data: str = request.args.get('data')
+    if data is None:
+        return jsonify("Please input a valid string: /local?data=test.png")
+    print("data ---- > ", data)
 
-        images: list[Image.Image] = loadLocalImage(imagePath)
-        if not images:
-            raise Exception("wrong Image get loaded: " + imagePath)
-        
-        results = predict_step(images)
-        return jsonify(results)
-    return jsonify("Not a proper request method or data")
+    realtiv_path = "images/" + data
+    image_path = os.path.join(os.getcwd(), realtiv_path)
+    images: list[Image.Image] = load_local_image(image_path)
+
+    if not images:
+        raise Exception("wrong Image get loaded: " + image_path)
+    results = predict_step(images)
+
+    return jsonify(results)   
+
 
 # Example:
 # http://127.0.0.1:5000/remote?url=https%3A%2F%2Fwww.test.de%2Fimage.png
 @app.route('/remote', methods=['GET'])
 def remote() -> Response:
-    url = request.args.get('url')
-    if url is None:
+    url_parameter = request.args.get('url')
+    if url_parameter is None:
         jsonify("Please input a valid string: /remote?url=https%3A%2F%2Fwww.test.de%2Fimage.png")
-    print("URL ---- > ", url)
-    
-    images: list[Image.Image] = loadRemoteImage(url)
+    print("URL ---- > ", url_parameter)
+
+    images: list[Image.Image] = load_remote_image(url_parameter)
     if not images:
-        raise Exception("wrong Image get loaded: " + url)
-    
+        raise Exception("wrong Image get loaded: " + url_parameter)
+
     results = predict_step(images)
     return jsonify(results)
 
 
-def loadRemoteImage(url: str) -> list[Image.Image]:
-    fileNameFromURIPattern: str = "/^.*\/(.*)\.(.*)\?.*$/"
-    foundFile = re.search(fileNameFromURIPattern, url)
-    file_name = foundFile.group()
-    if file_name is None:
-        raise Exception("Filename missing in URL")
-    
-    #download the remote file
-    res = requests.get(url, stream = True)
-    
-    #checking the responce for the requested remote file 
-    #and save to local disk
-    realtiv_path = "images/" + file_name
-    imagePath = os.path.join(os.getcwd(), realtiv_path)
-    try: 
+def load_remote_image(url: str) -> list[Image.Image]:
+    try:
         images: list[Image.Image] = []
-        if res.status_code == 200:
-            with open(imagePath,'wb') as f:
-                shutil.copyfileobj(res.raw, f)
-            print('Image sucessfully Downloaded: ',imagePath)
-        else:
-            raise Exception('Image Couldn\'t be retrieved')
-        
-        images.append(imagePath)
-        #check for empty list
-        if not images:
-            raise Exception("can not load image")
-        
-        return images
-    except OSError:
-        raise Exception("Image Not Found")
-    
+        img = Image.open(urlopen(url))
 
-def loadLocalImage(image_path: str) -> list[Image.Image]:
-    images: list[Image.Image] = []
-    try:        
-        loadedImage = Image.open(open(image_path, 'rb'))
-        if loadedImage.mode != "RGB":
-            loadedImage = loadedImage.convert(mode="RGB")            
-        images.append(loadedImage)
-        
-        #check for empty list
+        images.append(img)
+        # check for empty list
         if not images:
             raise Exception("can not load image")
-        
+
         return images
     except OSError:
         raise Exception("Image Not Found")
 
 
-# predict_step(['doctor.e16ba4e4.jpg']) # ['a woman in a hospital bed with a woman in a hospital bed']
+def load_local_image(image_path: str) -> list[Image.Image]:
+    try:
+        images: list[Image.Image] = []
+        loaded_image = Image.open(open(image_path, 'rb'))
+        if loaded_image.mode != "RGB":
+            loaded_image = loaded_image.convert(mode="RGB")
+        images.append(loaded_image)
+
+        # check for empty list
+        if not images:
+            raise Exception("can not load image")
+
+        return images
+    except OSError:
+        raise Exception("Image Not Found")
+
+
+# predict_step(['pexels-photo-5596193.jpeg']) # ['a gray and white cat sitting on top of a table']
 max_length = 16
 num_beams = 4
 gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
 def predict_step(image_list: list[Image.Image]) -> list[str]:
-    #build the token out of the image list
+    # build the token out of the image list
     pixel_values = feature_extractor(images=image_list, return_tensors="pt").pixel_values
     pixel_values = pixel_values.to(device)
 
@@ -111,7 +92,6 @@ def predict_step(image_list: list[Image.Image]) -> list[str]:
     preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
     preds = [pred.strip() for pred in preds]
     return preds
-
 
 
 if __name__ == '__main__':
@@ -123,7 +103,7 @@ if __name__ == '__main__':
     tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
     print("transformer tokenizer loaded")
 
-    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     device = torch.device("cpu")
     model.to(device)
     print("model loaded")
